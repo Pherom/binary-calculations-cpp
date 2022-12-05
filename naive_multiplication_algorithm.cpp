9e535f431ed18565de40d4c6047657c7bcd0ce28
Pherom <pheromem@gmail.com>
2022-12-05 18:57:07 +0200

    Created the naive multiplication algorithm

---
+++ naive_multiplication_algorithm.cpp
@@ -0,0 +1,21 @@
+#include "naive_multiplication_algorithm.h"
+#include "naive_addition_algorithm.h"
+
+BinaryNumber NaiveMultiplicationAlgorithm::calculate(const BinaryNumber& binary_number1, const BinaryNumber& binary_number2) {
+	BinaryNumber counter = binary_number1;
+	BinaryNumber result = binary_number2;
+	IAlgorithm* addition_algorithm = new NaiveAdditionAlgorithm();
+
+	if (counter != 0) {
+		--counter;
+	}
+
+	while (counter != 0) {
+		result = addition_algorithm->calculate(result, binary_number2);
+		--counter;
+	}
+
+	delete addition_algorithm;
+
+	return result;
+}
