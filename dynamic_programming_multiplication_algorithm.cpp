5404053dbb4cfdd36b2b179e424a5465053a6136
Elad Moshe <pheromem@gmail.com>
2022-12-08 02:06:21 +0200

    Fixed bugs in dynamic programming multiplication algorithm and karatsuba multiplication algorithm

--- dynamic_programming_multiplication_algorithm.cpp
+++ dynamic_programming_multiplication_algorithm.cpp
@@ -1,12 +1,12 @@
 #include "dynamic_programming_multiplication_algorithm.h"
 
 BinaryNumber DynamicProgrammingMultiplicationAlgorithm::calculate(const BinaryNumber& binary_number1, const BinaryNumber& binary_number2) {
-	if (binary_number1 <= 128 && binary_number2 <= 128) {
-		return BinaryNumber(binary_number1.getValueIfLessThanOrEqualTo128() * binary_number2.getValueIfLessThanOrEqualTo128());
+	if (binary_number1.logicalSize() <= 128 && binary_number2.logicalSize() <= 128) {
+		return termination_multiplication_algorithm->calculate(binary_number1, binary_number2);
 	}
 
 	BinaryNumber binary_number1_copy = binary_number1, binary_number2_copy = binary_number2;
-	size_t matched_size = BinaryNumber::matchSize(binary_number1_copy, binary_number2_copy);
+	size_t matched_size = BinaryNumber::matchSizeEven(binary_number1_copy, binary_number2_copy);
 
 	BinaryNumber binary_number1_first_half = binary_number1_copy.subBinaryNumber(0, matched_size / 2);
 	BinaryNumber binary_number1_second_half = binary_number1_copy.subBinaryNumber(matched_size / 2, matched_size);
@@ -21,9 +21,9 @@ BinaryNumber DynamicProgrammingMultiplicationAlgorithm::calculate(const BinaryNu
 
 	BinaryNumber mult_1fh_2sh_add_mult_1sh_2fh = addition_algorithm->calculate(mult_1fh_2sh, mult_1sh_2fh);
 	
-	BinaryNumber& addend1 = mult_1fh_2fh;
+	BinaryNumber& addend1 = mult_1fh_2fh.shiftLeft(matched_size);
 	BinaryNumber& addend2 = mult_1fh_2sh_add_mult_1sh_2fh.shiftLeft(matched_size / 2);
-	BinaryNumber& addend3 = mult_1sh_2sh.shiftLeft(matched_size);
+	BinaryNumber& addend3 = mult_1sh_2sh;
 
 	BinaryNumber result = addition_algorithm->calculate(addend1, addend2);
 	result = addition_algorithm->calculate(result, addend3);
